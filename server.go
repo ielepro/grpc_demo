37ef12f47a6d1ebf54694d92a2c3918241519236
nanxuezhi <1562985905@coolpad.com>
2019-05-30 10:25:52 +0800

    init

---
+++ server.go
@@ -0,0 +1,32 @@
+package k8s
+
+import (
+    "log"
+    "net"
+
+    pb "awesomeProject/helloworld"
+    "golang.org/x/net/context"
+    "google.golang.org/grpc"
+)
+
+const (
+    port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+    return &pb.HelloReply{Message: "Hello " + in.Name, Hello: "1", Word:"2"}, nil
+}
+
+func main() {
+    lis, err := net.Listen("tcp", port)
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+    s := grpc.NewServer()
+    pb.RegisterGreeterServer(s, &server{})
+    s.Serve(lis)
+}
